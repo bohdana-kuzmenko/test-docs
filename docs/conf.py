1528088e337675935e40bf671ca266cd5171bb42
bohdana_kuzmenko <bohdana.kuzmenko.work@gmail.com>
2019-07-22 18:27:41 +0300

    add master doc

--- docs/conf.py
+++ docs/conf.py
@@ -29,6 +29,8 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+master_doc = 'index'
+
 extensions = ['sphinx.ext.todo',
               'sphinx.ext.autodoc',
               'sphinx.ext.coverage']
