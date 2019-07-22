74ae104e5ddec2327c83229408968c46a0884bf4
bogdana <bohdana.kuzmenko.work@gmail.com>
2019-07-22 20:12:29 +0300

    change theme

--- docs/conf.py
+++ docs/conf.py
@@ -67,9 +67,9 @@ add_module_names = False
 # a list of builtin themes.
 
 
-html_theme = 'classic'
-# import sphinx_pdj_theme
-# html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
+import sphinx_pdj_theme
+html_theme = 'sphinx_pdj_theme'
+html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
