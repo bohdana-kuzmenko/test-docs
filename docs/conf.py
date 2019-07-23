81cd92d446b4918b2d5c1cce3f5423f9269f3a4c
bohdana_kuzmenko <bohdana.kuzmenko.work@gmail.com>
2019-07-23 10:09:30 +0300

    update styles

--- docs/conf.py
+++ docs/conf.py
@@ -75,5 +75,8 @@ html_theme_path = [msmb_theme.get_html_theme_path()]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = [
+    'css/style.css',
+]
 
 
